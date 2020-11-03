05f1bb1f7c7fde0496b2f5bb6503339c9b29b9de
Calvin Griffin <cgriffin332@gmail.com>
2020-11-03 18:19:20 -0500

    wrap in main tags

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,90 +2,93 @@ import React from "react";
 import gIcon from "../../images/github-icon_3.png";
 import lIcon from "../../images/linkedin-icon_3.png";
 
-
 const Contact = () => {
   return (
     <div>
-      <section className="container col-11 col-sm-7 col-lg-8 p-4 m-3 mx-auto bg-white">
-        <div className="row">
-          {/* <!-- page title --> */}
-          <div className="col-12 bg-white px-3">
-            <a href="https://github.com/cgriffin332" 
-            rel="noreferrer"
-            target="_blank">
-              <img
-                id="icon"
-                className="float-right mx-2"
-                src={gIcon}
-                alt="github icon"
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/calvin-griffin-8247521b7/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                id="icon"
-                className="float-right"
-                src={lIcon}
-                alt="linkedin icon"
-              />
-            </a>
-            <h1>Contact</h1>
-            <a
-              className="text-dark float-right"
-              href="https://docs.google.com/document/d/15ZuKnC6Rd6-zxL8zG5k4jfUj6cL0JcDl_nTcH423B6I/edit?usp=sharing"
-              el="noreferrer"
-              target="_blank"
-            >
-              View Resume
-            </a>
-            <p>Email: cgriffin332@gmail.com</p>
-            {/* <!-- contact form --> */}
-            <form>
-              {/* <!-- name input --> */}
-              <div className="mx-4 mt-4 form-group">
-                <label for="nameEntry">Name</label>
-                <input
-                  type="name"
-                  className="form-control inside-icon"
-                  placeholder="Name"
-                  id="name"
-                  aria-describedby="nameEntry"
+      <main>
+        <section className="container col-11 col-sm-7 col-lg-8 p-4 m-3 mx-auto bg-white">
+          <div className="row">
+            {/* <!-- page title --> */}
+            <div className="col-12 bg-white px-3">
+              <a
+                href="https://github.com/cgriffin332"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  id="icon"
+                  className="float-right mx-2"
+                  src={gIcon}
+                  alt="github icon"
                 />
-              </div>
-              {/* <!-- email input --> */}
-              <div className="mx-4 form-group">
-                <label for="exampleInputEmail1">Email</label>
-                <input
-                  type="email"
-                  className="form-control inside-icon"
-                  placeholder="Email"
-                  id="email"
-                  aria-describedby="emailContact"
+              </a>
+              <a
+                href="https://www.linkedin.com/in/calvin-griffin-8247521b7/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  id="icon"
+                  className="float-right"
+                  src={lIcon}
+                  alt="linkedin icon"
                 />
-              </div>
-              {/* <!-- message input --> */}
-              <div className="mx-4 mb-4 form-group">
-                <label for="exampleFormControlTextarea1">Message</label>
-                <textarea
-                  className="form-control"
-                  placeholder="Message"
-                  id="message"
-                  rows="4"
-                ></textarea>
-              </div>
-              <div>
-                {/* <!-- submit button --> */}
-                <button className="submitBtn" type="submit">
-                  Submit
-                </button>
-              </div>
-            </form>
+              </a>
+              <h1>Contact</h1>
+              <a
+                className="text-dark float-right"
+                href="https://docs.google.com/document/d/15ZuKnC6Rd6-zxL8zG5k4jfUj6cL0JcDl_nTcH423B6I/edit?usp=sharing"
+                el="noreferrer"
+                target="_blank"
+              >
+                View Resume
+              </a>
+              <p>Email: cgriffin332@gmail.com</p>
+              {/* <!-- contact form --> */}
+              <form>
+                {/* <!-- name input --> */}
+                <div className="mx-4 mt-4 form-group">
+                  <label for="nameEntry">Name</label>
+                  <input
+                    type="name"
+                    className="form-control inside-icon"
+                    placeholder="Name"
+                    id="name"
+                    aria-describedby="nameEntry"
+                  />
+                </div>
+                {/* <!-- email input --> */}
+                <div className="mx-4 form-group">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input
+                    type="email"
+                    className="form-control inside-icon"
+                    placeholder="Email"
+                    id="email"
+                    aria-describedby="emailContact"
+                  />
+                </div>
+                {/* <!-- message input --> */}
+                <div className="mx-4 mb-4 form-group">
+                  <label for="exampleFormControlTextarea1">Message</label>
+                  <textarea
+                    className="form-control"
+                    placeholder="Message"
+                    id="message"
+                    rows="4"
+                  ></textarea>
+                </div>
+                <div>
+                  {/* <!-- submit button --> */}
+                  <button className="submitBtn" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </main>
     </div>
   );
 };
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -11,190 +11,203 @@ import DateNight from "../../images/datenight.png";
 const Portfolio = () => {
   return (
     <div>
-      <section className="container col-11 col-sm-7 col-lg-8 p-4 m-3 mx-auto bg-white">
-        <div className="row">
-          <div className="col-12 px-3 bg-white">
-            {/* <!-- page title --> */}
-            <a href="https://github.com/cgriffin332"
-            rel="noreferrer"
-            target="_blank">
-              <img
-                id="icon"
-                className="float-right mx-2"
-                src={gIcon}
-                alt="github icon"
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/calvin-griffin-8247521b7/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <img
-                id="icon"
-                className="float-right"
-                src={lIcon}
-                alt="linkedin icon"
-              />
-            </a>
-            <h1>Portfolio</h1>
-          </div>
-        </div>
-        {/* <!-- portfolio --> */}
-        <div className="row">
-          <div className="col-lg-6 p-3 bg-white">
-            {/* <!-- project 5 --> */}
-            <div className="card">
-              <a href="https://burger-time332.herokuapp.com/" rel="noreferrer" target="_blank">
-                <img
-                  className="card-img-top"
-                  src={Burger}
-                  alt="Card image cap"
-                />
-              </a>
-              <div className="card-body">
-                <h5 className="card-title">Burger Time</h5>
-                <a
-                  href="https://github.com/cgriffin332/burger-time332"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Github Link
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* <!-- blank space --> */}
-          <div className="col-lg-6 p-3 bg-white">
-            <div className="card">
-              <a href="https://gifted332.herokuapp.com/" rel="noreferrer"
-              target="_blank">
-                <img
-                  className="card-img-top"
-                  src={Gifted}
-                  alt="Card image cap"
-                />
-              </a>
-              <div className="card-body">
-                <h5 className="card-title">Burger Time</h5>
-                <a href="https://github.com/cgriffin332/gifted" 
-                rel="noreferrer"
-                target="_blank">
-                  Github Link
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-6 p-3 bg-white">
-            {/* <!-- project 1 --> */}
-            <div className="card">
+      <main>
+        <section className="container col-11 col-sm-7 col-lg-8 p-4 m-3 mx-auto bg-white">
+          <div className="row">
+            <div className="col-12 px-3 bg-white">
+              {/* <!-- page title --> */}
               <a
-                href="https://cgriffin332.github.io/Weather-Dashboard332/"
+                href="https://github.com/cgriffin332"
                 rel="noreferrer"
                 target="_blank"
               >
                 <img
-                  className="card-img-top"
-                  src={Weather}
-                  alt="Card image cap"
+                  id="icon"
+                  className="float-right mx-2"
+                  src={gIcon}
+                  alt="github icon"
                 />
               </a>
-              <div className="card-body">
-                <h5 className="card-title">Weather Dashboard</h5>
-                <a
-                  href="https://github.com/cgriffin332/Weather-Dashboard332"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Github Link
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6 p-3 bg-white">
-            {/* <!-- project 2 --> */}
-            <div className="card">
               <a
-                href="https://cgriffin332.github.io/Work-Day-Scheduler332/"
+                href="https://www.linkedin.com/in/calvin-griffin-8247521b7/"
                 rel="noreferrer"
                 target="_blank"
               >
                 <img
-                  className="card-img-top"
-                  src={Workweek}
-                  alt="Card image cap"
+                  id="icon"
+                  className="float-right"
+                  src={lIcon}
+                  alt="linkedin icon"
                 />
               </a>
-              <div className="card-body">
-                <h5 className="card-title">Work Day Planner</h5>
+              <h1>Portfolio</h1>
+            </div>
+          </div>
+          {/* <!-- portfolio --> */}
+          <div className="row">
+            <div className="col-lg-6 p-3 bg-white">
+              {/* <!-- project 5 --> */}
+              <div className="card">
                 <a
-                  href="https://github.com/cgriffin332/Work-Day-Scheduler332"
+                  href="https://burger-time332.herokuapp.com/"
                   rel="noreferrer"
                   target="_blank"
                 >
-                  Github Link
+                  <img
+                    className="card-img-top"
+                    src={Burger}
+                    alt="Card image cap"
+                  />
                 </a>
+                <div className="card-body">
+                  <h5 className="card-title">Burger Time</h5>
+                  <a
+                    href="https://github.com/cgriffin332/burger-time332"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Github Link
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-6 p-3 bg-white">
-            {/* <!-- project 3 --> */}
-            <div className="card">
-              <a
-                href="https://cgriffin332.github.io/Code-Quiz332/#"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <img
-                  className="card-img-top"
-                  src={Quiz}
-                  alt="Card image cap"
-                />
-              </a>
-              <div className="card-body">
-                <h5 className="card-title">Code Quiz</h5>
+            {/* <!-- blank space --> */}
+            <div className="col-lg-6 p-3 bg-white">
+              <div className="card">
                 <a
-                  href="https://github.com/cgriffin332/Code-Quiz332"
+                  href="https://gifted332.herokuapp.com/"
                   rel="noreferrer"
                   target="_blank"
                 >
-                  Github Link
+                  <img
+                    className="card-img-top"
+                    src={Gifted}
+                    alt="Card image cap"
+                  />
                 </a>
+                <div className="card-body">
+                  <h5 className="card-title">Burger Time</h5>
+                  <a
+                    href="https://github.com/cgriffin332/gifted"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Github Link
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-lg-6 p-3 bg-white">
-            {/* <!-- project 4 --> */}
-            <div className="card">
-              <a
-                href="https://cgriffin332.github.io/The-Date-Night-Planner/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <img
-                  className="card-img-top"
-                  src={DateNight}
-                  alt="Card image cap"
-                />
-              </a>
-              <div className="card-body">
-                <h5 className="card-title">Date Night Planner</h5>
+          <div className="row">
+            <div className="col-lg-6 p-3 bg-white">
+              {/* <!-- project 1 --> */}
+              <div className="card">
                 <a
-                  href="https://github.com/cgriffin332/The-Date-Night-Planner"
+                  href="https://cgriffin332.github.io/Weather-Dashboard332/"
                   rel="noreferrer"
                   target="_blank"
                 >
-                  Github Link
+                  <img
+                    className="card-img-top"
+                    src={Weather}
+                    alt="Card image cap"
+                  />
                 </a>
+                <div className="card-body">
+                  <h5 className="card-title">Weather Dashboard</h5>
+                  <a
+                    href="https://github.com/cgriffin332/Weather-Dashboard332"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Github Link
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 p-3 bg-white">
+              {/* <!-- project 2 --> */}
+              <div className="card">
+                <a
+                  href="https://cgriffin332.github.io/Work-Day-Scheduler332/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <img
+                    className="card-img-top"
+                    src={Workweek}
+                    alt="Card image cap"
+                  />
+                </a>
+                <div className="card-body">
+                  <h5 className="card-title">Work Day Planner</h5>
+                  <a
+                    href="https://github.com/cgriffin332/Work-Day-Scheduler332"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Github Link
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+          <div className="row">
+            <div className="col-lg-6 p-3 bg-white">
+              {/* <!-- project 3 --> */}
+              <div className="card">
+                <a
+                  href="https://cgriffin332.github.io/Code-Quiz332/#"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <img
+                    className="card-img-top"
+                    src={Quiz}
+                    alt="Card image cap"
+                  />
+                </a>
+                <div className="card-body">
+                  <h5 className="card-title">Code Quiz</h5>
+                  <a
+                    href="https://github.com/cgriffin332/Code-Quiz332"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Github Link
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 p-3 bg-white">
+              {/* <!-- project 4 --> */}
+              <div className="card">
+                <a
+                  href="https://cgriffin332.github.io/The-Date-Night-Planner/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <img
+                    className="card-img-top"
+                    src={DateNight}
+                    alt="Card image cap"
+                  />
+                </a>
+                <div className="card-body">
+                  <h5 className="card-title">Date Night Planner</h5>
+                  <a
+                    href="https://github.com/cgriffin332/The-Date-Night-Planner"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Github Link
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
