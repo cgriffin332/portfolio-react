13109726f56658fde63786e5cac41795884c5cb9
Calvin Griffin <cgriffin332@gmail.com>
2020-11-03 00:00:22 -0500

    set up navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        {/* <!-- navbar --> */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          {/* <!-- name on navbar --> */}
+          <a className="navbar-brand nav-name text-white">Calvin Griffin</a>
+          {/* <!-- collapse toggle button --> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {/* <!-- About, Portfolio, Contact links on right of navbar --> */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item ml-auto">
+                {/* <!-- link to contact page --> */}
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item ml-auto">
+                {/* <!-- link to portfolio page --> */}
+                <Link className="nav-link" to="/portfolio">
+                  Portfolio
+                </Link>
+              </li>
+              {/* <!-- active page --> */}
+              <li className="nav-item ml-auto active">
+                {/* <!-- link to about page --> */}
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
