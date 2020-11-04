08af5c0031cc2a92aae1429c83f6cdb86dad9cab
Calvin Griffin <cgriffin332@gmail.com>
2020-11-03 22:21:28 -0500

    component out first two projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Project from "./Project";
 import gIcon from "../../images/github-icon_3.png";
 import lIcon from "../../images/linkedin-icon_3.png";
 import Burger from "../../images/burger3.png";
@@ -9,6 +10,20 @@ import Quiz from "../../images/wrong.png";
 import DateNight from "../../images/datenight.png";
 
 const Portfolio = () => {
+  const projects = [
+    {
+      href: "https://burger-time332.herokuapp.com/",
+      image: Burger,
+      title: "Gifted",
+      github: "https://github.com/cgriffin332/burger-time332",
+    },
+    {
+      href: "https://gifted332.herokuapp.com/",
+      image: Gifted,
+      title: "Burger Time",
+      github: "https://github.com/cgriffin332/gifted",
+    },
+  ];
   return (
     <div>
       <main>
@@ -45,58 +60,20 @@ const Portfolio = () => {
           </div>
           {/* <!-- portfolio --> */}
           <div className="row">
-            <div className="col-lg-6 p-3 bg-white">
-              {/* <!-- project 5 --> */}
-              <div className="card">
-                <a
-                  href="https://burger-time332.herokuapp.com/"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    className="card-img-top"
-                    src={Burger}
-                    alt="Card image cap"
-                  />
-                </a>
-                <div className="card-body">
-                  <h5 className="card-title">Burger Time</h5>
-                  <a
-                    href="https://github.com/cgriffin332/burger-time332"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    Github Link
-                  </a>
-                </div>
-              </div>
-            </div>
+            {/* <!-- project 5 --> */}
+            <Project
+              href={projects[0].href}
+              image={projects[0].image}
+              title={projects[0].title}
+              github={projects[0].github}
+            />
             {/* <!-- blank space --> */}
-            <div className="col-lg-6 p-3 bg-white">
-              <div className="card">
-                <a
-                  href="https://gifted332.herokuapp.com/"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  <img
-                    className="card-img-top"
-                    src={Gifted}
-                    alt="Card image cap"
-                  />
-                </a>
-                <div className="card-body">
-                  <h5 className="card-title">Burger Time</h5>
-                  <a
-                    href="https://github.com/cgriffin332/gifted"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    Github Link
-                  </a>
-                </div>
-              </div>
-            </div>
+            <Project
+              href={projects[1].href}
+              image={projects[1].image}
+              title={projects[1].title}
+              github={projects[1].github}
+            />
           </div>
           <div className="row">
             <div className="col-lg-6 p-3 bg-white">
--- src/components/Portfolio/Project.jsx
+++ src/components/Portfolio/Project.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 
-const projects = [
-  {
-    href: "https://burger-time332.herokuapp.com/",
-    image: "",
-    title: "Burger Time",
-    github: "https://github.com/cgriffin332/burger-time332",
-  },
-];
-
 const Project = ({ href, image, title, github }) => {
   return (
-    <div className="card">
-      <a href={href} rel="noreferrer" target="_blank">
-        <img className="card-img-top" src={image} alt="Card image cap" />
-      </a>
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <a href={github} rel="noreferrer" target="_blank">
-          Github Link
+    <div className="col-lg-6 p-3 bg-white">
+      <div className="card">
+        <a href={href} rel="noreferrer" target="_blank">
+          <img className="card-img-top" src={image} alt="Card image cap" />
         </a>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <a href={github} rel="noreferrer" target="_blank">
+            Github Link
+          </a>
+        </div>
       </div>
     </div>
   );
